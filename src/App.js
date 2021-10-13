334677dcc6896fd91510d93129936ad6d0c0936e
Luis-Paulo-Silva-Reis <lpreis2010@gmail.com>
2021-10-13 13:58:16 -0300

    !

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './style.css';
 import Item from '../components/Item';
-import Card from '../components/Card'
+import Card from '../components/Card';
 
 export default function App() {
   return (
     <>
       <h1>minha primeira aplicação react</h1>
       <ul>
-        <Item > Item 1 </Item>
-        <Item > Item 2 </Item>
-        <Item > Item 3 </Item>
+        <Item> Item 1 </Item>
+        <Item> Item 2 </Item>
+        <Item> Item 3 </Item>
       </ul>
-      <Card/>
+      <Card />
     </>
   );
 }
